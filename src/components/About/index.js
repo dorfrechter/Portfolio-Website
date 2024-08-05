30394dd8433271fb488c1ac12085a60ff0ebdecc
dor.frechter <dor.frechter@gmail.com>
2024-08-05 15:32:45 -0400

    a

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -27,7 +27,7 @@ const About = () => {
                     I am an aspiring software engineer with a passion for machine learning, data science, and creating full stack applications. I am currently a student at the School of Engineering of the University of Virginia, where I am pursuing a B.S in Computer Science. 
                 </p>
                 <p>
-                    I was born in Bitzaron, Israel and grew up there for 13 years. Then, I moved to New York where I had to adapt to the new culture, learn a new language, and make new relationships. Outside of work, I am an outdoors enthusiast. I enjoy hiking, biking, skiing, climbing or backpacking, and I am always looking for new adventures!
+                    I was born in Bitzaron, Israel and spent the first 13 years of my life there. Then, I moved to New York where I had to adapt to the new culture, learn a new language, and make new relationships. Outside of work, I am an outdoors enthusiast. I enjoy hiking, biking, skiing, climbing or backpacking, and I am always looking for new adventures!
                 </p>
                 <p>
                     Here are some cool pictures of me and some friends from my recent adventures!
